18ceda7ad6da6e7867e139490467c29e4173a5c6
Thomas Bellembois <thomas.bellembois@uca.fr>
2024-05-27 14:08:29 +0200

    First commit.

---
+++ src/searchable.rs
@@ -0,0 +1,17 @@
+pub trait Searchable {
+    fn new(&self) -> Self;
+
+    fn set_exact_search(&mut self, match_exact_search: bool);
+    fn get_exact_search(&self) -> bool;
+
+    fn get_table_name(&self) -> String;
+
+    fn get_id_field_name(&self) -> String;
+    fn get_text_field_name(&self) -> String;
+
+    fn set_id_field(&mut self, id: u64);
+    fn set_text_field(&mut self, text: &str);
+
+    fn get_id(&self) -> u64;
+    fn get_text(&self) -> String;
+}
